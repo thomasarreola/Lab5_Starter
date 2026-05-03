c1cc533f6a50ecb1453a2f2bab7168e6fd927739
Thomas Arreola <thomasarreolaprofessional@gmail.com>
2026-05-02 17:01:50 -0700

    made it so the basic horn selection stuff works which includes when you change the horn it changes the display image and also the audio that cna play. I also made it so when you click the button it plays the selected horn

--- assets/scripts/expose.js
+++ assets/scripts/expose.js
@@ -3,5 +3,26 @@
 window.addEventListener('DOMContentLoaded', init);
 
 function init() {
-  // TODO
+  const dropdown = document.getElementById("horn-select");
+  const displayImage = document.querySelector("header + img")
+  const sound = document.querySelector("audio");
+  const button = document.querySelector("button");
+
+  dropdown.addEventListener('change', function() {
+    if(dropdown.value == "air-horn"){
+      displayImage.src = "assets/images/air-horn.svg";
+      sound.src = "assets/audio/air-horn.mp3";
+    }else if(dropdown.value == "car-horn"){
+      displayImage.src = "assets/images/car-horn.svg";
+      sound.src = "assets/audio/car-horn.mp3";
+    }else if(dropdown.value == "party-horn"){
+      displayImage.src = "assets/images/party-horn.svg";
+      sound.src = "assets/audio/party-horn.mp3";
+    }else{
+      displayImage.src = "assets/images/no-image.png";
+    }
+  });
+  button.addEventListener('click', function(){
+    sound.play();
+  });
 }
